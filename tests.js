aec502417b4d2312c24aede908faa4ce68a79646
Stephen Wu <stefan.wu@gmail.com>
2015-10-11 18:12:35 -0700

    Inserted examples of mocha testing into tests.js   - For efficiency!   - And Science!

--- tests.js
+++ tests.js
@@ -1,1 +1,25 @@
-// Specs bruh
+describe('testingTheThing', function() {
+  // var thing;
+
+  ////--------DO SOMETHING BEFORE EACH it()-----------------
+  // beforeEach(function() {
+  //   binarySearchTree = BinarySearchTree(5);
+  // });
+
+  // it('should have methods named "insert", "contains", and "depthFirstLog', function() {
+  //   expect().to.be.a();
+  //   expect().to.equal();
+  // });
+
+  it('test1', function(){
+
+  });
+
+  it('test3', function(){
+
+  });
+
+  it('test2', function(){
+
+  });
+});
